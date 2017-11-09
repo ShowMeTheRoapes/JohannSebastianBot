ad31f44979581f4e57e3df485b2fa837384c4786
ahoopes16 <kah4627@truman.edu>
2017-11-08 23:40:50 -0600

    Added some basic useful Midiutil programming stuff

--- bot.py
+++ bot.py
@@ -16,3 +16,46 @@ The game plan:
 
   We will meet in 2 days to confirm
   '''
+
+
+
+
+
+#_____________BASE FOR HOW TO ADD NOTES AND CREATE A MIDIFILE_________________#
+"""
+#Import the library
+from midiutil.MidiFile import MIDIFile
+ 
+
+# Create the MIDIFile Object with 1 track
+MyMIDI = MIDIFile(1)
+ 
+
+# Tracks are numbered from zero. Times are measured in beats.
+track = 0   
+time = 0
+ 
+
+# Add track name and tempo.
+MyMIDI.addTrackName(track,time,"Sample Track")
+MyMIDI.addTempo(track,time,120)
+ 
+
+# Add a note. addNote expects the following information:
+track = 0
+channel = 0
+pitch = 60
+time = 0
+duration = 1
+volume = 100
+ 
+
+# Now add the note.
+MyMIDI.addNote(track,channel,pitch,time,duration,volume)
+ 
+
+# And write it to disk.
+binfile = open("output.mid", 'wb')
+MyMIDI.writeFile(binfile)
+binfile.close()
+"""
