cb511f807fa138a98447d633caba9faf48d2cda3
Jeremy Schmich <jeremy.schmich@gmail.com>
2017-11-08 20:45:44 -0600

    adds the game plan

---
+++ bot.py
@@ -0,0 +1,18 @@
+def newMethod():
+  print("hello world")
+
+def initializePopulation(size):
+  pass
+
+#an individual is 16 beats, each beat can contain zero or more notes
+#a note has a pitch and a duration
+#
+
+'''
+The game plan:
+  Jeremy will be in charge of finding out how to do evaluation
+  Alex will work on midi util and all that 
+  Dustin will decide on note representation and individuals representation, and crossover, etc. reproduction
+
+  We will meet in 2 days to confirm
+  '''
