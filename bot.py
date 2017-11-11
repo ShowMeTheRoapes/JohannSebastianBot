2dc2d37de63035024b7044889ddbffd78ccbaf1e
ahoopes16 <kah4627@truman.edu>
2017-11-10 19:44:03 -0600

    Added how to play a midi file and read a midi file

--- bot.py
+++ bot.py
@@ -1,22 +1,31 @@
-def newMethod():
-  print("hello world")
+# TODO Take Bach MIDI files and get them in a usable format
+# TODO Create initial random population
+# TODO while (not done)
+# TODO   Evaluate individuals
+# TODO   Crossover elite individuals and generate next population
+# TODO   Mutate (if needed)
 
-def initializePopulation(size):
-  pass
-
-#an individual is 16 beats, each beat can contain zero or more notes
-#a note has a pitch and a duration
-#
+import midiutil
+import mido
+import pygame
 
 '''
 The game plan:
   Jeremy will be in charge of finding out how to do evaluation
-  Alex will work on midi util and all that 
+  Alex will work on midi util and all that
   Dustin will decide on note representation and individuals representation, and crossover, etc. reproduction
 
   We will meet in 2 days to confirm
-  '''
+'''
 
+
+def initialize_population(size):
+  pass
+
+# note = [pitch (48-84), time(0-16), duration]
+# individual = [16 notes]
+# an individual is 16 beats, each beat can contain zero or more notes
+# a note consists of a pitch and a duration
 
 
 
@@ -59,3 +68,20 @@ binfile = open("output.mid", 'wb')
 MyMIDI.writeFile(binfile)
 binfile.close()
 """
+
+#__________________HOW TO READ A MIDI FILE_____________#
+'''
+file = mido.MidiFile("bwv906a.mid")
+for msg in file.play():
+    print(msg)
+'''
+
+#___________________HOW TO PLAY A MIDI FILE_____________#
+'''
+pygame.init()
+pygame.mixer.music.load("bwv906a.mid")
+pygame.mixer.music.play(0)
+
+while pygame.mixer.music.get_busy():
+    pygame.event.poll()
+'''
