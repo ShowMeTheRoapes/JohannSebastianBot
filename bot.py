b33958100915c205fe551a04c32b4e7cae65262c
ahoopes16 <kah4627@truman.edu>
2017-11-27 20:14:35 -0600

    -cleaned up some comments and stuff

--- bot.py
+++ bot.py
@@ -1,13 +1,5 @@
-# TODO Create initial random population
-# TODO while (not done)
-# TODO   Evaluate individuals
-# TODO   Crossover elite individuals and generate next population
-# TODO   Mutate (if needed)
-
 # Creating midi files
 from midiutil.MidiFile import MIDIFile
-# Reading midi files
-import mido
 # Playing midi files
 import pygame
 import math as Math
@@ -19,88 +11,6 @@ PARENT_PERCENT = 0.25
 RANDOM_PERCENT = 0.10
 MUTANT_PERCENT = 0.40
 MUTATE_NUM = 8
-
-# _____________________REPRODUCTION AND MUTATION INFO_________________________#
-"""
-Reproduction/Next Gen:
-  Top scoring individuals will reproduce for next round.
-  Bottom individuals will be replaced with a new randomized individual
-  A few of the other tops will stay with a few mutations
-  Proportions of new gen assuming 10 per gen: 2/10 top reproducers, 1/10 bottom one re-randomized, 2/10 lesser tops with definite mutations, 5/10 new children with possible mutations
-  1
-Crossover:
-  Cross over will be a random assignment that should be about 50%
-    INDIVIDUAL_SIZE = ???
-    ...
-    parent0 = [...]
-    parent1 = [...]
-    child = []
-    for i in range(INDIVIDUAL_SIZE):
-        if randint(0,1) == 0:
-            child.append(parent0[i])
-        else:
-            child.append(parent1[i])
-
-Mutation:
-  Mutation will be a lowish percentage that just completely randomizes the note that is being mutated...maybe like 5-10% chance for new children
-
-"""
-
-# _____________BASE FOR HOW TO ADD NOTES AND CREATE A MIDIFILE_________________#
-"""
-#Import the library
-from midiutil.MidiFile import MIDIFile
-
-
-# Create the MIDIFile Object with 1 track
-MyMIDI = MIDIFile(1)
-
-
-# Tracks are numbered from zero. Times are measured in beats.
-track = 0   
-time = 0
-
-
-# Add track name and tempo.
-MyMIDI.addTrackName(track,time,"Sample Track")
-MyMIDI.addTempo(track,time,120)
-
-
-# Add a note. addNote expects the following information:
-track = 0    (constant)
-channel = 0  (constant)
-pitch = 60
-time = 0
-duration = 1
-volume = 100 (constant)
-
-
-# Now add the note.
-MyMIDI.addNote(track,channel,pitch,time,duration,volume)
-
-
-# And write it to disk.
-binfile = open("output.mid", 'wb')
-MyMIDI.writeFile(binfile)
-binfile.close()
-"""
-
-# __________________HOW TO READ A MIDI FILE_____________#
-'''
-file = mido.MidiFile("bwv906a.mid")
-for msg in file.play():
-    print(msg)
-'''
-
-# ___________________HOW TO PLAY A MIDI FILE_____________#
-'''
-pygame.init()
-pygame.mixer.music.load("bwv906a.mid")
-pygame.mixer.music.play(0)
-
-while pygame.mixer.music.get_busy():
-    pygame.event.poll()
-'''
 
 
 def play_midi_file(songfile):
